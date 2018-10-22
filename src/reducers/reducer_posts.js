ac916b2b470d5e337768a8dddbc8121be431aa63
Luxi <luxi.lindsey@gmail.com>
2018-10-22 01:26:26 -0700

    Created new action in action creator DELETE_POST and applied the new action to the container posts_show.js and the reducer. Added a button and more navigation links to home page.

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
 
   switch (action.type) {
+  case DELETE_POST:
+    return _.omit(state, action.payload);
   case FETCH_POST:
   // ES5
     // const post = action.payload.data
@@ -13,7 +15,7 @@ export default function(state = {}, action) {
     // return newState;
 
   // ES6 syntax
-  return { ...state, [action.payload.data.id]: action.payload.data }; 
+  return { ...state, [action.payload.data.id]: action.payload.data };
   case FETCH_POSTS:
     return _.mapKeys(action.payload.data, 'id');
   default:
